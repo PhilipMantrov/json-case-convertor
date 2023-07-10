d428e686f79d8035767debebb83af519be9d24d4
Philip Mantrov <philip.mantrov@gmail.com>
2023-07-10 17:55:42 +0300

    ts ver.1

--- index.js
+++ index.js
@@ -1,286 +1,246 @@
-//License Type: ISC
-//Author: Mir Taha Ali
-const _ = require('lodash');
+// License Type: ISC
+// Original Author: Mir Taha Ali
+// Fork Author: Philip Mantrov
 
-const caseCondition = (origKey, keyCase) => {
-	let newKey = null;
-	switch (keyCase){
-		case 'pascal':
-			newKey = _.startCase(_.camelCase(origKey)).replace(/ /g, '');//(origKey.charAt(0).toUpperCase() + origKey.slice(1) || origKey).toString()
-			break;
-		case 'camel':
-			newKey = _.camelCase(origKey)
-			break;
-		case 'kebab':
-			newKey = _.kebabCase(origKey)
-			break;
-		case 'snake':
-			newKey = _.snakeCase(origKey)
-			break;
-		case 'upper':
-			newKey = _.toUpper(origKey)
-			break;
-		case'constant':
-			newKey = _.upperCase(origKey).replace(/ /g, '_')
-			break;
-		case 'dot':
-			newKey = _.lowerCase(origKey).replace(/ /g, '.')
-			break;
-		case 'path':
-			newKey = _.lowerCase(origKey).replace(/ /g, '/')
-			break;
-		case 'lower':
-			newKey = _.toLower(origKey)
-			break;
-		case 'sentence':
-			newKey = _.upperFirst(_.lowerCase(origKey));
-			break;
-		case 'title':
-			newKey = _.startCase(_.camelCase(origKey));
-			break;
-	}
-	return newKey;
+import _ from 'lodash';
+
+const caseCondition = (origKey, keyCase): string => {
+  let newKey = null;
+  switch (keyCase){
+    case `pascal`:
+      newKey = _.startCase(_.camelCase(origKey)).replace(/ /g, ``);
+      break;
+    case `camel`:
+      newKey = _.camelCase(origKey)
+      break;
+    case `kebab`:
+      newKey = _.kebabCase(origKey)
+      break;
+    case `snake`:
+      newKey = _.snakeCase(origKey)
+      break;
+    case `upper`:
+      newKey = _.toUpper(origKey)
+      break;
+    case`constant`:
+      newKey = _.upperCase(origKey).replace(/ /g, `_`)
+      break;
+    case `dot`:
+      newKey = _.lowerCase(origKey).replace(/ /g, `.`)
+      break;
+    case `path`:
+      newKey = _.lowerCase(origKey).replace(/ /g, `/`)
+      break;
+    case `lower`:
+      newKey = _.toLower(origKey)
+      break;
+    case `sentence`:
+      newKey = _.upperFirst(_.lowerCase(origKey));
+      break;
+    case `title`:
+      newKey = _.startCase(_.camelCase(origKey));
+      break;
+  }
+  return newKey;
 }
 
-function keyConvertor(obj, keyCase) {
-	if (typeof obj !== 'object' && obj !== null) {
-		return obj;
-	}
-	let objectToSend, origKey, newKey, value
-	if (obj instanceof Array) {
-		return obj.map(function (value) {
-			if (typeof value === "object") {
-				value = keyConvertor(value, keyCase)
-			}
-			return value
-		})
-	} else {
-		objectToSend = {}
-		for (origKey in obj) {
-			if (obj.hasOwnProperty(origKey)) {
-				newKey = caseCondition(origKey, keyCase);
-				value = obj[origKey]
-				if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
-					value = keyConvertor(value, keyCase)
-				}
-				objectToSend[newKey] = value
-			}
-		}
-	}
-	return objectToSend
+function keyConvertor(obj, keyCase): any {
+  if (typeof obj !== `object` && obj !== null) {
+    return obj;
+  }
+  let objectToSend, origKey, newKey, value
+  if (obj instanceof Array) {
+    return obj.map(function (value) {
+      if (typeof value === `object`) {
+        value = keyConvertor(value, keyCase)
+      }
+      return value
+    })
+  } else {
+    objectToSend = {}
+    for (origKey in obj) {
+      if (obj.hasOwnProperty(origKey)) {
+        newKey = caseCondition(origKey, keyCase);
+        value = obj[origKey]
+        if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
+          value = keyConvertor(value, keyCase)
+        }
+        objectToSend[newKey] = value
+      }
+    }
+  }
+  return objectToSend
 }
 
-function allConvertor(obj, keyCase) {
-	if (typeof obj !== 'object' && obj !== null) {
-		return obj;
-	}
-	let objectToSend, origKey, newKey, value
-	if (obj instanceof Array) {
-		return obj.map(function (value) {
-			if (typeof value === "object") {
-				value = allConvertor(value, keyCase)
-			}
-			if (typeof value === 'string')
-			{
-				value = caseCondition(value, keyCase);
-			}
-			return value
-		})
-	} else {
-		objectToSend = {}
-		for (origKey in obj) {
-			if (obj.hasOwnProperty(origKey)) {
-				newKey = caseCondition(origKey, keyCase);
-				value = obj[origKey]
-				if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
-					value = allConvertor(value, keyCase)
-				}
-				else {
-					if (typeof value === 'string')
-						value = caseCondition(value, keyCase);
-				}
-				objectToSend[newKey] = value
-			}
-		}
-	}
-	return objectToSend
+function allConvertor(obj, keyCase): any {
+  if (typeof obj !== `object` && obj !== null) {
+    return obj;
+  }
+  let objectToSend, origKey, newKey, value
+  if (obj instanceof Array) {
+    return obj.map(function (value) {
+      if (typeof value === `object`) {
+        value = allConvertor(value, keyCase)
+      }
+      if (typeof value === `string`)
+      {
+        value = caseCondition(value, keyCase);
+      }
+      return value
+    })
+  } else {
+    objectToSend = {}
+    for (origKey in obj) {
+      if (obj.hasOwnProperty(origKey)) {
+        newKey = caseCondition(origKey, keyCase);
+        value = obj[origKey]
+        if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
+          value = allConvertor(value, keyCase)
+        }
+        else {
+          if (typeof value === `string`)
+            value = caseCondition(value, keyCase);
+        }
+        objectToSend[newKey] = value
+      }
+    }
+  }
+  return objectToSend
 }
 
-function valueConvertor(obj, keyCase) {
-	if (typeof obj !== 'object' && obj !== null) {
-		if (typeof obj !== 'string')
-			return caseCondition(obj);
-		else return obj;
-	}
-	let objectToSend, origKey, value
-	if (obj instanceof Array) {
-		return obj.map(function (value) {
-			if (typeof value === "object") {
-				value = valueConvertor(value, keyCase)
-			}
-			if (typeof value === 'string')
-			{
-				value = caseCondition(value, keyCase);
-			}
-			return value
-		})
-	} else {
-		objectToSend = {}
-		for (origKey in obj) {
-			if (obj.hasOwnProperty(origKey)) {
-				value = obj[origKey]
-				if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
-					value = valueConvertor(value, keyCase)
-				}
-				else {
-					if (typeof value === 'string')
-						value = caseCondition(value, keyCase);
-				}
-				objectToSend[origKey] = value
-			}
-		}
-	}
-	return objectToSend
+function valueConvertor(obj, keyCase): any {
+  if (typeof obj !== `object` && obj !== null) {
+    if (typeof obj !== `string`)
+      return caseCondition(obj, keyCase );
+    else return obj;
+  }
+  let objectToSend, origKey, value
+  if (obj instanceof Array) {
+    return obj.map(function (value) {
+      if (typeof value === `object`) {
+        value = valueConvertor(value, keyCase)
+      }
+      if (typeof value === `string`)
+      {
+        value = caseCondition(value, keyCase);
+      }
+      return value
+    })
+  } else {
+    objectToSend = {}
+    for (origKey in obj) {
+      if (obj.hasOwnProperty(origKey)) {
+        value = obj[origKey]
+        if (value instanceof Array || (value !== null && value !== undefined && value.constructor === Object)) {
+          value = valueConvertor(value, keyCase)
+        }
+        else {
+          if (typeof value === `string`)
+            value = caseCondition(value, keyCase);
+        }
+        objectToSend[origKey] = value
+      }
+    }
+  }
+  return objectToSend
 }
-
 //-----------------------------------------------
-const pascalCaseKeys = (obj) => {
-	return keyConvertor(obj, 'pascal')
+export function pascalCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `pascal`)
 }
-const camelCaseKeys = (obj) => {
-	return keyConvertor(obj, 'camel')
+export function camelCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `camel`)
 }
-const snakeCaseKeys = (obj) => {
-	return keyConvertor(obj, 'snake')
+export function snakeCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `snake`)
 }
-const kebabCaseKeys = (obj) => {
-	return keyConvertor(obj, 'kebab')
+export function kebabCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `kebab`)
 }
-const upperCaseKeys = (obj) => {
-	return keyConvertor(obj, 'upper')
+export function upperCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `upper`)
 }
-const lowerCaseKeys = (obj) => {
-	return keyConvertor(obj, 'lower')
+export function lowerCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `lower`)
 }
-const constantCaseKeys = (obj) => {
-	return keyConvertor(obj, 'constant')
+export function constantCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `constant`)
 }
-const dotCaseKeys = (obj) => {
-	return keyConvertor(obj, 'dot')
+export function dotCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `dot`)
 }
-const pathCaseKeys = (obj) => {
-	return keyConvertor(obj, 'path')
+export function pathCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `path`)
 }
-const sentenceCaseKeys = (obj) => {
-	return keyConvertor(obj, 'sentence')
+export function sentenceCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `sentence`)
 }
-const titleCaseKeys = (obj) => {
-	return keyConvertor(obj, 'title')
+export function titleCaseKeys<T>(obj: any): T {
+  return keyConvertor(obj, `title`)
 }
-//-------------------------------------------------
-const pascalCaseValues = (obj) => {
-	return valueConvertor(obj, 'pascal')
+export function pascalCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `pascal`)
 }
-const camelCaseValues = (obj) => {
-	return valueConvertor(obj, 'camel')
+export function camelCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `camel`)
 }
-const snakeCaseValues = (obj) => {
-	return valueConvertor(obj, 'snake')
+export function snakeCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `snake`)
 }
-const kebabCaseValues = (obj) => {
-	return valueConvertor(obj, 'kebab')
+export function kebabCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `kebab`)
 }
-const upperCaseValues = (obj) => {
-	return valueConvertor(obj, 'upper')
+export function upperCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `upper`)
 }
-const lowerCaseValues = (obj) => {
-	return valueConvertor(obj, 'lower')
+export function lowerCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `lower`)
 }
-const constantCaseValues = (obj) => {
-	return valueConvertor(obj, 'constant')
+export function constantCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `constant`)
 }
-const dotCaseValues = (obj) => {
-	return valueConvertor(obj, 'dot')
+export function dotCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `dot`)
 }
-const pathCaseValues = (obj) => {
-	return valueConvertor(obj, 'path')
+export function pathCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `path`)
 }
-const sentenceCaseValues = (obj) => {
-	return valueConvertor(obj, 'sentence')
+export function sentenceCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `sentence`)
 }
-const titleCaseValues = (obj) => {
-	return valueConvertor(obj, 'title')
+export function titleCaseValues<T>(obj: any): T {
+  return valueConvertor(obj, `title`)
 }
-//-------------------------------------------------
-const pascalCaseAll = (obj) => {
-	return allConvertor(obj, 'pascal')
+export function pascalCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `pascal`)
 }
-const camelCaseAll = (obj) => {
-	return allConvertor(obj, 'camel')
+export function camelCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `camel`)
 }
-const snakeCaseAll = (obj) => {
-	return allConvertor(obj, 'snake')
+export function snakeCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `snake`)
 }
-const kebabCaseAll = (obj) => {
-	return allConvertor(obj, 'kebab')
+export function kebabCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `kebab`)
 }
-const upperCaseAll = (obj) => {
-	return allConvertor(obj, 'upper')
+export function upperCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `upper`)
 }
-const lowerCaseAll = (obj) => {
-	return allConvertor(obj, 'lower')
+export function lowerCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `lower`)
 }
-const constantCaseAll = (obj) => {
-	return allConvertor(obj, 'constant')
+export function constantCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `constant`)
 }
-const dotCaseAll = (obj) => {
-	return allConvertor(obj, 'dot')
+export function dotCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `dot`)
 }
-const pathCaseAll = (obj) => {
-	return allConvertor(obj, 'path')
+export function pathCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `path`)
 }
-const sentenceCaseAll = (obj) => {
-	return allConvertor(obj, 'sentence')
+export function sentenceCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `sentence`)
 }
-const titleCaseAll = (obj) => {
-	return allConvertor(obj, 'title')
+export function titleCaseAll<T>(obj: any): T {
+  return allConvertor(obj, `title`)
 }
-//----------------------------------------------
-
-module.exports = {
-	pascalCaseKeys,
-	camelCaseKeys,
-	snakeCaseKeys,
-	kebabCaseKeys,
-	upperCaseKeys,
-	lowerCaseKeys,
-	constantCaseKeys,
-	dotCaseKeys,
-	pathCaseKeys,
-	sentenceCaseKeys,
-	titleCaseKeys,
-	//-------------------
-	pascalCaseAll,
-	camelCaseAll,
-	snakeCaseAll,
-	kebabCaseAll,
-	upperCaseAll,
-	lowerCaseAll,
-	constantCaseAll,
-	dotCaseAll,
-	pathCaseAll,
-	sentenceCaseAll,
-	titleCaseAll,
-	//---------------------
-	pascalCaseValues,
-	camelCaseValues,
-	snakeCaseValues,
-	kebabCaseValues,
-	upperCaseValues,
-	lowerCaseValues,
-	constantCaseValues,
-	dotCaseValues,
-	pathCaseValues,
-	sentenceCaseValues,
-	titleCaseValues
-};
